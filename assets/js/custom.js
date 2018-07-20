637278a7751102c7259752380c314f4d1440a093
pr08 <pr08@figd-1711708.figdtk.de>
2018-07-20 15:32:24 +0200

    2nd Commmit 20.07.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,30 @@
 (function () {
-
-    http://192.168.0.22:8383/20_navigator/index.html
-            //
-            //
-//https://developer.mozilla.org/en-US/docs/Web/API/Coordinates
-            var getPos = function (pos) {
-                  alert(2);
-                var mapLink = document.querySelector('#mapLink');
-                var latlon = pos.coords.latitude + ',' + pos.coords.longitude;
-
-                var url = 'https://www.google.de/maps/@' + latlon + ',14z';
-                mapLink.href = url;
-                mapLink.innerText = url;
-                console.log(pos);
-            };
-
-    if (navigator.geolocation) {
-      
-        navigator.geolocation.getCurrentPosition(getPos);
-    }
-
-
+//    var getPos = function (pos) {        
+//        var mapLink = document.querySelector('#mapLink');
+//        var latlon = pos.coords.latitude + ',' + pos.coords.longitude;
+//        var position1 = pos.coords.latitude;
+//        var position2 = pos.coords.longitude;
+//        var url = 'https://www.google.de/maps/@' + latlon + ',14z';        
+//        mapLink.href = url;
+//        mapLink.innerText = url;
+//        getPosition(position1, position2);
+//        
+//        
+//    };
+    var googleMap = document.querySelector('#googleMap');
+    var getPosition = function(){
+        console.log(navigator);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(initMap);
+        }          
+    };     
+    var initMap = function(pos){         
+        var opts ={
+          zoom:8,
+          center:new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude)
+        };
+        //ruft eine neue Map auf und speichert sie in der id googleMap
+        new google.maps.Map(googleMap, opts); 
+    };
+    google.maps.event.addDomListener(window,'load', getPosition);    
 })();
